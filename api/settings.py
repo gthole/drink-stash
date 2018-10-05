901a05f77445ac77add5cf067393c0f42cbb600d
Greg Thole <gthole@gmail.com>
2018-10-05 10:44:53 -0400

    Long token ttl

--- api/settings.py
+++ api/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=365),
 }
 
 MIDDLEWARE_CLASSES = [
