e5a74b4aa341577fb44e9941a93d316306769517
Greg Thole <gthole@gmail.com>
2018-11-21 15:00:23 -0500

    Add recipe admin

--- api/drinks/admin.py
+++ api/drinks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import Ingredient, Quantity, Recipe
 
 
 @admin.register(Ingredient)
@@ -7,5 +7,26 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'generic')
     search_fields = ['name']
     ordering = ['name']
+
+
+class QuantityInline(admin.TabularInline):
+    model = Quantity
+    min_num = 2
+    extra = 3
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        QuantityInline,
+    ]
+
+    exclude = ['added_by',]
+
+    def save_model(self, request, instance, form, change):
+        if not change:
+            self.added_by = request.user
+        super(RecipeAdmin, self).save_model(request, instance, form, change)
+
 
 admin.site.site_header = 'Drink Stash Admin'
