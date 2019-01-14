83fe981e2e83d2fe18fa15a3a6a07aae4fc7b49f
Greg Thole <gthole@gmail.com>
2019-01-14 10:04:44 -0500

    Redirect root level favicons to assets

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework import routers
 
@@ -18,6 +20,14 @@ urlpatterns = [
     url(r'^api/v1/auth/', obtain_jwt_token),
     url(r'^api/v1/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon.ico$', RedirectView.as_view(
+        url=settings.STATIC_URL + 'assets/favicon.ico',
+        permanent=True
+    )),
+    url(r'^apple-touch-icon.png$', RedirectView.as_view(
+        url=settings.STATIC_URL + 'assets/apple-touch-icon.png',
+        permanent=True
+    )),
     url(r'', index),  # Angular routing
     url(r'^$', index),  # Angular routing
 ]
