2a2a68f51f392870aa4663eccab14f4dd60b79ba
Greg Thole <gthole@gmail.com>
2019-01-14 21:51:45 -0500

    Allow non-admins

--- api/settings.py
+++ api/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
