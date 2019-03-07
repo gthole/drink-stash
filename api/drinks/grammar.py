ef0a29a26effd7c5bcbae6df53b2cfe5589f8d12
Greg Thole <gthole@gmail.com>
2019-03-07 18:55:46 -0500

    Expand grammar for more fun searching

--- api/drinks/grammar.py
+++ api/drinks/grammar.py
@@ -11,19 +11,33 @@ OPS = {
     '>=': 'gte',
     '<=': 'lte'
 }
+
+# Attributes - these can never be a search term
+ALIASES = {
+    'comments': 'comment_count',
+    'favorites': 'favorite_count',
+    'desc': 'description',
+    'description': 'description',
+    'source': 'source',
+    'name': 'name',
+    'directions': 'directions'
+}
 unit_keys = '"i|"'.join([k for k in UNITS.keys() if k])
+exclude_keys = '|'.join([k for k in ALIASES.keys()])
 
 grammar = Lark('''
     // Top level grammar rules
-    query: SEARCH_TERM OPERATOR NUMBER [UNIT] -> constraint
+    query: NUM_ATTR OPERATOR NUMBER -> attr_constraint
+          | SEARCH_TERM OPERATOR NUMBER [UNIT] -> constraint
           | SEARCH_TERM -> search_term
           | ATTR "=" SEARCH_TERM -> attr_search
           | "NOT" SEARCH_TERM -> exclude
 
     // Tokens
     COMBINER: ("AND" | "OR")
-    ATTR.2: ("name" | "description" | "directions")
-    SEARCH_TERM: /((?!AND|OR|NOT)\w)+(\s((?!AND|OR|NOT)\w)+)*/
+    NUM_ATTR.2: ("comments" | "favorites")
+    ATTR: ("name" | "description" | "directions" | "source")
+    SEARCH_TERM: /((?!AND|OR|NOT|%s)[^<=> ])+(\s((?!AND|OR|NOT)[^<=> ])+)*/
     OPERATOR: ("<="|">="|"="|"<"|">")
     UNIT: ("%s"i)
     WHITESPACE: (" " | "\\n")+
@@ -32,7 +46,14 @@ grammar = Lark('''
     // Imports and configs
     %%import common.INT -> INT
     %%ignore WHITESPACE
-''' % (unit_keys,) , start='query')
+''' % (exclude_keys, unit_keys) , start='query')
+
+def parse_number(token):
+    if "/" in token:
+        [num, den] = token.split("/")
+        return float(num) / float(den)
+    else:
+        return float(token)
 
 
 def parse_search_and_filter(term, qs):
@@ -57,16 +78,18 @@ def parse_search_and_filter(term, qs):
             Q(description__iregex='\\b%s\\b' % term)
         )
 
+    if tree.data == 'attr_constraint':
+        attr = ALIASES.get(tree.children[0], tree.children[0])
+        op = OPS['%s' % tree.children[1]]
+        amount = parse_number(tree.children[2])
+        kwargs = {}
+        kwargs['%s__%s' % (attr, op)] = amount
+        return qs.filter(**kwargs)
+
     if tree.data == 'constraint':
         search = '%s' % tree.children[0]
         op = OPS['%s' % tree.children[1]]
-
-        amount_str = tree.children[2]
-        if "/" in amount_str:
-            [num, den] = amount_str.split("/")
-            amount = float(num) / float(den)
-        else:
-            amount = float(tree.children[2])
+        amount = parse_number(tree.children[2])
 
         if len(tree.children) == 4:
             unit = UNITS[tree.children[3].lower()]
@@ -77,10 +100,10 @@ def parse_search_and_filter(term, qs):
             'quantity__unit': unit
         }
         kwargs['quantity__amount__%s' % op] = amount
-
         return qs.filter(**kwargs)
 
     if tree.data == 'attr_search':
+        attr = ALIASES.get(tree.children[0], tree.children[0])
         kwargs = {}
         kwargs['%s__icontains' % tree.children[0]] = '%s' % tree.children[1]
         return qs.filter(**kwargs)
