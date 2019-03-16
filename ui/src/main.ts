635396bd4ad42d2904c60b8f6f6eb386ab8046dc
Greg Thole <gthole@gmail.com>
2019-03-15 21:02:27 -0400

    Live reload in dev env

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (!environment.production) {
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>')
+}
 enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
