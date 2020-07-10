19d8226bb886cab844798d0dfa8676e5067ebc1c
Greg Thole <gthole@gmail.com>
2020-07-10 18:56:38 -0400

    Better ingredient row linebreak solution

--- ui/src/components/IngredientRow/index.js
+++ ui/src/components/IngredientRow/index.js
@@ -3,11 +3,16 @@ import './style.css';
 
 export function IngredientRow({ingredients}) {
     if (!ingredients) return '';
+    // Use React.Fragment here to prevent crazy line breaks due to stacking
+    // spans together without spaces between them
     return (
         <div className="IngredientRow">
             {
                 ingredients.map((ingredient, i) => (
-                    <span key={'q-' + i}>{ingredient}</span>
+                    <React.Fragment>
+                        <span key={'q-' + i}>{ingredient}</span>
+                        { ' ' }
+                    </React.Fragment>
                 ))
             }
         </div>
