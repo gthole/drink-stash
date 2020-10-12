d234da0a525f60fda9f7c24aece40b0f384671da
Greg Thole <gthole@gmail.com>
2020-10-12 11:23:28 -0400

    Parametrize CSS colors and add a Dark Mode

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './themes.css';
 import './style.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
